b30bbe4bc689161bd75d3a559a2de323c751afa5
Mariya <marixasha@mail.ru>
2025-04-10 01:25:52 +0300

    Initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,22 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
+  let d = b**2 - 4 * a * c;
   
-  return arr;
+  if (d < 0) {
+    arr = [];
+  } else if (d === 0) {
+    arr = [-b / (2 * a)];
+  } else if (d > 0) {
+    arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
+  } 
+    return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let monthlyRate = (percent / 100) / 12;
+  let loanBody = amount - contribution;
+  let monthlyPayment = (loanBody * (monthlyRate + (monthlyRate / (((1 + monthlyRate) ** countMonths) - 1))));
+  let totalAmount = Number((monthlyPayment * countMonths).toFixed(2));
+    return totalAmount;
 }
